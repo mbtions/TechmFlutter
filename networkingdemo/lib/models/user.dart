90294717071bc3799b1f99b5846bad1ec80f285f
mbtions <workbymeenakshi@gmail.com>
2025-06-11 15:53:10 +0530

    updated redundant code

--- networkingdemo/lib/models/user.dart
+++ networkingdemo/lib/models/user.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'address.dart';
-import 'company.dart';
 
 User userFromJson(String str) => User.fromJson(json.decode(str));
 
@@ -53,5 +51,69 @@ class User {
     "phone": phone,
     "website": website,
     "company": company.toJson(),
+  };
+}
+
+class Address {
+  String street;
+  String suite;
+  String city;
+  String zipcode;
+  Geo geo;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.geo,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) => Address(
+    street: json["street"],
+    suite: json["suite"],
+    city: json["city"],
+    zipcode: json["zipcode"],
+    geo: Geo.fromJson(json["geo"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "street": street,
+    "suite": suite,
+    "city": city,
+    "zipcode": zipcode,
+    "geo": geo.toJson(),
+  };
+}
+
+class Geo {
+  String lat;
+  String lng;
+
+  Geo({required this.lat, required this.lng});
+
+  factory Geo.fromJson(Map<String, dynamic> json) =>
+      Geo(lat: json["lat"], lng: json["lng"]);
+
+  Map<String, dynamic> toJson() => {"lat": lat, "lng": lng};
+}
+
+class Company {
+  String name;
+  String catchPhrase;
+  String bs;
+
+  Company({required this.name, required this.catchPhrase, required this.bs});
+
+  factory Company.fromJson(Map<String, dynamic> json) => Company(
+    name: json["name"],
+    catchPhrase: json["catchPhrase"],
+    bs: json["bs"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "catchPhrase": catchPhrase,
+    "bs": bs,
   };
 }
